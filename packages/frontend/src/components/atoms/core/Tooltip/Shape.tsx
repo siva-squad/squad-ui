16554b51ef5260f83b9251adcc95f332e25de6ac
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2023-10-02 17:26:47 +0900

    Add direction functionality and svg shape

---
+++ packages/frontend/src/components/atoms/core/Tooltip/Shape.tsx
@@ -0,0 +1,17 @@
+export const Shape = ({ className }: { className: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="10"
+      height="5"
+      viewBox="0 0 10 5"
+      fill="none"
+      className={className}
+    >
+      <path
+        d="M5 5L0 0H10L5 5Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
--- packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
+++ packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
@@ -1,11 +1,28 @@
 //import { clsx } from "clsx";
 
+import { Shape } from "./Shape";
 import type { TooltipProps } from "./type";
 
 export const Tooltip = ({ direction, position, text }: TooltipProps) => {
+  const directionClasses = getDirectionClass(direction);
+
   return (
-    <div className="inline-flex items-center justify-center rounded p-2 text-sm leading-normal shadow-04">
+    <div className="relative inline-flex items-center justify-center rounded p-2 text-sm leading-normal shadow-04">
       {text}
+      <Shape className="absolute -right-1.5 m-auto -rotate-90" />
     </div>
   );
 };
+
+function getDirectionClass(direction: string) {
+  switch (direction) {
+    case "bottom":
+      return "-bottom-1";
+    case "top":
+      return "-top-1 rotate-180";
+    case "left":
+      return "-left-1.5 rotate-90";
+    case "right":
+      return "-right-1.5 -rotate-90";
+  }
+}
