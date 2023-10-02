35ade7225bd7c642ac88aa3d24f64b0db95d6da4
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2023-10-02 15:10:01 +0900

    Add Figma HTML and Tailwind

--- packages/frontend/src/components/atoms/core/Tooltip/Tooltip.stories.tsx
+++ packages/frontend/src/components/atoms/core/Tooltip/Tooltip.stories.tsx
@@ -11,7 +11,7 @@ export default {
 
 export const Default: StoryObj<typeof Tooltip> = {
   argTypes: {},
-  args: {},
+  args: {
+    text: "メッセージテキスト",
+  },
 };
-
-
--- packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
+++ packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
@@ -1,7 +1,15 @@
-import { clsx } from 'clsx';
+import { clsx } from "clsx";
 
-import type { TooltipProps } from './type';
+import type { TooltipProps } from "./type";
 
-export const Tooltip = ({}: TooltipProps) => {
-  return <></>;
-}
+export const Tooltip = ({ direction, position, text }: TooltipProps) => {
+  return (
+    <div className="shadow inline-flex h-11 w-36 flex-col items-center justify-center">
+      <div className="bg-white inline-flex items-center justify-start gap-2.5 rounded p-2">
+        <div className="text-neutral-900 font-['Noto Sans CJK JP'] text-sm font-normal leading-snug">
+          {text}
+        </div>
+      </div>
+    </div>
+  );
+};
