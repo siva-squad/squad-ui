02a8c0636761c345ba0995f006a60deed590f0d7
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2023-10-02 16:05:42 +0900

    Refactor generated HTML and CSS

--- packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
+++ packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
@@ -1,15 +1,11 @@
-import { clsx } from "clsx";
+//import { clsx } from "clsx";
 
 import type { TooltipProps } from "./type";
 
 export const Tooltip = ({ direction, position, text }: TooltipProps) => {
   return (
-    <div className="shadow inline-flex h-11 w-36 flex-col items-center justify-center">
-      <div className="bg-white inline-flex items-center justify-start gap-2.5 rounded p-2">
-        <div className="text-neutral-900 font-['Noto Sans CJK JP'] text-sm font-normal leading-snug">
-          {text}
-        </div>
-      </div>
+    <div className="inline-flex items-center justify-center rounded p-2 text-sm leading-normal shadow-04">
+      {text}
     </div>
   );
 };
