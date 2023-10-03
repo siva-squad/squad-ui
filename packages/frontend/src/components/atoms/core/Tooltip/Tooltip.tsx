53abd4584b822c6d7cfff23e80f38f5242922b64
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2023-10-03 16:10:23 +0900

    Extract utility functions

--- packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
+++ packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
@@ -2,10 +2,12 @@ import { clsx } from "clsx";
 
 import { Shape } from "./Shape";
 import type { TooltipProps } from "./type";
+import { getDirectionClass, getPositionClass } from "./utils";
 
 export const Tooltip = ({ direction, position, text }: TooltipProps) => {
   const directionClasses = getDirectionClass(direction);
-  const positionClasses = getPositionClass(position); // if direction === left or right should only be center!!
+  const positionClasses =
+    direction === "left" || direction === "right" ? "" : getPositionClass(position);
 
   return (
     <div className="relative inline-flex items-center justify-center rounded p-2 text-sm leading-normal shadow-04">
@@ -14,26 +16,3 @@ export const Tooltip = ({ direction, position, text }: TooltipProps) => {
     </div>
   );
 };
-
-function getDirectionClass(direction: string) {
-  switch (direction) {
-    case "bottom":
-      return "-bottom-1";
-    case "top":
-      return "-top-1 rotate-180";
-    case "left":
-      return "left-[-7px] rotate-90";
-    case "right":
-      return "right-[-7px] -rotate-90";
-  }
-}
-function getPositionClass(position: string) {
-  switch (position) {
-    case "center":
-      return "";
-    case "left":
-      return "left-1/4";
-    case "right":
-      return "right-1/4";
-  }
-}
--- packages/frontend/src/components/atoms/core/Tooltip/type.ts
+++ packages/frontend/src/components/atoms/core/Tooltip/type.ts
@@ -1,2 +1,9 @@
+export type Direction = "bottom" | "top" | "right" | "left";
+
+export type Position = "left" | "right" | "center";
+
 export type TooltipProps = {
-}
+  direction: Direction;
+  position: Position;
+  text: string;
+};
