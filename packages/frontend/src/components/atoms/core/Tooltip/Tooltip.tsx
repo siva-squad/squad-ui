4f5521c877948ca93f6998dbe3f1dd20fd7b8d4f
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2023-10-05 15:51:00 +0900

    Change util functions with consts

--- packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
+++ packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
@@ -1,18 +1,16 @@
 import { clsx } from "clsx";
 
+import { DIRECTION_STYLE, POSITION_STYLE } from "./consts";
 import { Shape } from "./Shape";
 import type { TooltipProps } from "./type";
-import { getDirectionClass, getPositionClass } from "./utils";
 
 export const Tooltip = ({ direction, position, text }: TooltipProps) => {
-  const directionClasses = getDirectionClass(direction);
-  const positionClasses =
-    direction === "left" || direction === "right" ? "" : getPositionClass(position);
-
   return (
     <div className="relative inline-flex items-center justify-center rounded p-2 text-sm leading-normal shadow-04">
       {text}
-      <Shape className={clsx("absolute m-auto", directionClasses, positionClasses)} />
+      <Shape
+        className={clsx("absolute m-auto", DIRECTION_STYLE[direction], POSITION_STYLE[position])}
+      />
     </div>
   );
 };
---
+++ packages/frontend/src/components/atoms/core/Tooltip/consts.ts
@@ -0,0 +1,14 @@
+import { TooltipProps } from "./type";
+
+export const DIRECTION_STYLE: { [key in TooltipProps["direction"]]: string } = {
+  bottom: "-bottom-1",
+  top: "-top-1 rotate-180",
+  left: "left-[-7px] rotate-90",
+  right: "right-[-7px] -rotate-90",
+} as const;
+
+export const POSITION_STYLE: { [key in TooltipProps["position"]]: string } = {
+  center: "",
+  left: "left-1/4",
+  right: "right-1/4",
+} as const;
