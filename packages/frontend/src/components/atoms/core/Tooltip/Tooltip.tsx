82bee82dfacd50b45703b0477a5bf937be955f10
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2023-10-06 11:14:29 +0900

    Add hover state to open tooltip

--- packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
+++ packages/frontend/src/components/atoms/core/Tooltip/Tooltip.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { clsx } from "clsx";
 
 import { DIRECTION_STYLE, POSITION_STYLE } from "./consts";
@@ -5,17 +6,26 @@ import { Shape } from "./Shape";
 import type { TooltipProps } from "./type";
 
 export const Tooltip = ({ direction, position, tooltipText, children }: TooltipProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const positionStyles =
     direction === "left" || direction === "right" ? "" : POSITION_STYLE[position];
 
   return (
-    <div>
-      <div className="inline-flex rounded text-sm leading-normal shadow-04">
-        <span className="relative inline-flex h-full w-full items-center justify-center p-2">
-          {tooltipText}
-          <Shape className={clsx("absolute m-auto", DIRECTION_STYLE[direction], positionStyles)} />
-        </span>
-      </div>
+    <div
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+    >
+      {isOpen && (
+        <div className="inline-flex rounded text-sm leading-normal shadow-04">
+          <span className="relative inline-flex h-full w-full items-center justify-center p-2">
+            {tooltipText}
+            <Shape
+              className={clsx("absolute m-auto", DIRECTION_STYLE[direction], positionStyles)}
+            />
+          </span>
+        </div>
+      )}
       {children}
     </div>
   );
