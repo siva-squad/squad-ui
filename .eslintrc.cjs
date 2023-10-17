1fe6a8f8d5f7777fedc3212b2b3cdceef55d6a5b
yoheeeei <yohei.contact@gmail.com>
2023-10-17 18:40:31 +0900

    chore: 📦 add vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,15 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ["prettier", "plugin:tailwindcss/recommended", "plugin:storybook/recommended", "eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
+  extends: [
+    "prettier",
+    "plugin:tailwindcss/recommended",
+    "plugin:storybook/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+  ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh", "react", "react-hooks", "tailwindcss"],
@@ -9,7 +17,7 @@ module.exports = {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     // Rules for third party plugins
     "import/order": [
-          "error",
+      "error",
       {
         groups: ["builtin", "external", "parent", "sibling", "index"],
         alphabetize: { order: "asc", caseInsensitive: true },
