0fae1be490da6508943860fb95c8c1a9d2e14967
yoheeeei <yohei.contact@gmail.com>
2023-10-17 12:06:16 +0900

    chore(eslint): :wrench: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,40 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: ["plugin:tailwindcss/recommended", "plugin:storybook/recommended", "eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "react", "react-hooks"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    // Rules for third party plugins
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "parent", "sibling", "index"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+        pathGroups: [
+          // sort by react* → react*/** → recoil ...
+          {
+            pattern: "react*",
+            group: "builtin",
+            position: "before",
+          },
+          {
+            pattern: "react*/**",
+            group: "builtin",
+            position: "before",
+          },
+          {
+            pattern: "recoil",
+            group: "builtin",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+      },
     ],
+    "react-hooks/exhaustive-deps": "warn",
+    "no-irregular-whitespace": ["error", { skipTemplates: true }],
   },
-}
+};
