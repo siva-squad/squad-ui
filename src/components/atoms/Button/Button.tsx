0b0790527491a093a7d47bc9d8bc2074933de6d0
yoheeeei <yohei.contact@gmail.com>
2023-10-18 11:51:00 +0900

    chore: 📦 add clsx, tailwind-variants

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,5 +1,8 @@
+import clsx from "clsx";
 import { ButtonProp } from "./type";
 
 export const Button = ({ children }: ButtonProp) => (
-  <button className="rounded-full bg-black px-4 py-2 font-bold text-white">{children}</button>
+  <button className={clsx("rounded-full bg-black px-4 py-2 font-bold text-white")}>
+    {children}
+  </button>
 );
