7c3ece2c026ed105d84c37aa106fffd9fc4479a8
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-10-24 17:06:25 +0900

    動的タグ対応

--- src/components/molecules/MenuListItem/MenuListItem.tsx
+++ src/components/molecules/MenuListItem/MenuListItem.tsx
@@ -9,7 +9,10 @@ import {
 } from "./const";
 import type { MenuListItemProps } from "./type";
 
-export const MenuListItem = React.forwardRef<HTMLAnchorElement, MenuListItemProps>(
+export const MenuListItem = React.forwardRef<
+  HTMLAnchorElement | HTMLButtonElement,
+  MenuListItemProps
+>(
   (
     {
       type = "dropdown",
@@ -25,6 +28,8 @@ export const MenuListItem = React.forwardRef<HTMLAnchorElement, MenuListItemProp
     },
     ref,
   ) => {
+    const Action = href ? "a" : "button";
+
     const iconUI = useMemo(() => {
       if (selected) {
         return selectedIcon || icon;
@@ -33,41 +38,22 @@ export const MenuListItem = React.forwardRef<HTMLAnchorElement, MenuListItemProp
       return icon;
     }, [icon, selected, selectedIcon]);
 
-    const commonProps = useMemo(() => {
-      return {
-        className: MENU_LIST_ITEM_CONTAINER_CLASS_NAME({ selected, type }),
-        disabled,
-        onClick,
-      };
-    }, [disabled, onClick, selected, type]);
-
-    const commonChildren = useMemo(() => {
-      return (
-        <>
-          {icon && <span className={MENU_LIST_ITEM_ICON_CLASS_NAME({ selected })}>{iconUI}</span>}
-          <div className="flex flex-col items-start">
-            <span className={MENU_LIST_ITEM_TEXT_CLASS_NAME({ selected })}>{children}</span>
-            <span className={MENU_LIST_ITEM_DESCRIPTION_CLASS_NAME({ selected })}>
-              {description}
-            </span>
-          </div>
-          {chevron && <ChevronDownIcon className="h-4 w-4 text-gray-dark" />}
-        </>
-      );
-    }, [chevron, children, description, icon, iconUI, selected]);
-
-    if (href)
-      return (
-        <a
-          href={href}
-          ref={ref}
-          {...commonProps}
-        >
-          {commonChildren}
-        </a>
-      );
-
-    return <button {...commonProps}>{commonChildren}</button>;
+    return (
+      <Action
+        className={MENU_LIST_ITEM_CONTAINER_CLASS_NAME({ selected, type })}
+        disabled={disabled}
+        onClick={onClick}
+        ref={ref as React.RefObject<HTMLAnchorElement> & React.RefObject<HTMLButtonElement>}
+        {...(href && { href })}
+      >
+        {icon && <span className={MENU_LIST_ITEM_ICON_CLASS_NAME({ selected })}>{iconUI}</span>}
+        <div className="flex flex-col items-start">
+          <span className={MENU_LIST_ITEM_TEXT_CLASS_NAME({ selected })}>{children}</span>
+          <span className={MENU_LIST_ITEM_DESCRIPTION_CLASS_NAME({ selected })}>{description}</span>
+        </div>
+        {chevron && <ChevronDownIcon className="h-4 w-4 text-gray-dark" />}
+      </Action>
+    );
   },
 );
 
