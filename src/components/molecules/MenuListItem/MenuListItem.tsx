742e06e742b46f5f370f61d26e78d2ee0b00f3cf
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-10-24 12:53:46 +0900

    tailwind-variants化

--- src/components/molecules/MenuListItem/MenuListItem.tsx
+++ src/components/molecules/MenuListItem/MenuListItem.tsx
@@ -1,5 +1,9 @@
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
-import clsx from "clsx";
+import {
+  MENU_LIST_ITEM_CONTAINER_CLASS_NAME,
+  MENU_LIST_ITEM_ICON_CLASS_NAME,
+  MENU_LIST_ITEM_TEXT_CLASS_NAME,
+} from "./const";
 import type { MenuListItemProps } from "./type";
 
 export const MenuListItem = ({
@@ -14,37 +18,12 @@ export const MenuListItem = ({
 }: MenuListItemProps) => {
   return (
     <button
-      className={clsx(
-        "group flex h-8 w-fit items-center gap-x-2 rounded px-2",
-        "hover:bg-gray-extraLight",
-        "disabled:cursor-not-allowed disabled:opacity-20",
-        "border-primary-600 focus:border",
-        "outline-none",
-        { "bg-primary-50": selected },
-      )}
+      className={MENU_LIST_ITEM_CONTAINER_CLASS_NAME({ selected })}
       disabled={disabled}
       onClick={onClick}
     >
-      {icon && (
-        <span
-          className={clsx("h-4 w-4 text-gray group-enabled:group-active:text-primary-600", {
-            "text-primary-600": selected,
-          })}
-        >
-          {icon}
-        </span>
-      )}
-      <span
-        className={clsx(
-          "text-sm font-medium text-gray-dark",
-          "group-enabled:group-active:text-primary-600",
-          {
-            "text-primary-600": selected,
-          },
-        )}
-      >
-        {children}
-      </span>
+      {icon && <span className={MENU_LIST_ITEM_ICON_CLASS_NAME({ selected })}>{icon}</span>}
+      <span className={MENU_LIST_ITEM_TEXT_CLASS_NAME({ selected })}>{children}</span>
       {chevron && <ChevronDownIcon className="h-4 w-4 text-gray-dark" />}
     </button>
   );
---
+++ src/components/molecules/MenuListItem/const.ts
@@ -0,0 +1,37 @@
+import clsx from "clsx";
+import { tv } from "tailwind-variants";
+
+export const MENU_LIST_ITEM_CONTAINER_CLASS_NAME = tv({
+  base: clsx(
+    "group flex h-8 w-fit items-center gap-x-2 rounded px-2 outline-none",
+    "hover:bg-gray-extraLight",
+    "disabled:cursor-not-allowed disabled:opacity-20",
+    "focus:border focus:border-primary-600",
+  ),
+  variants: {
+    selected: {
+      true: "bg-primary-50",
+      false: "",
+    },
+  },
+});
+
+export const MENU_LIST_ITEM_TEXT_CLASS_NAME = tv({
+  base: clsx("text-sm font-medium text-gray-dark", "group-enabled:group-active:text-primary-600"),
+  variants: {
+    selected: {
+      true: "text-primary-600",
+      false: "",
+    },
+  },
+});
+
+export const MENU_LIST_ITEM_ICON_CLASS_NAME = tv({
+  base: clsx("h-4 w-4 text-gray", "group-enabled:group-active:text-primary-600"),
+  variants: {
+    selected: {
+      true: "text-primary-600",
+      false: "",
+    },
+  },
+});
