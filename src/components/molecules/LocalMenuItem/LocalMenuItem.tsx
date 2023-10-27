0aaadc858e8c1d4c47fd2ebef95f1b5b46883b12
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-10-27 09:23:19 +0900

    refの受け取り追加
    NextLink対応

--- src/components/molecules/LocalMenuItem/LocalMenuItem.tsx
+++ src/components/molecules/LocalMenuItem/LocalMenuItem.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from "react";
+import React from "react";
 import {
   MENUITEM_CONTAINER_CLASS_NAME,
   MENUITEM_ICON_CLASS_NAME,
@@ -6,30 +7,28 @@ import {
 } from "./const";
 import type { LocalMenuItemProps } from "./type";
 
-export const LocalMenuItem = ({
-  icon,
-  selectedIcon,
-  title,
-  isDisabled = false,
-  isSelected = false,
-  onClick,
-}: LocalMenuItemProps) => {
-  const iconUI = useMemo(() => {
-    if (isSelected) {
-      return selectedIcon || icon;
-    }
+export const LocalMenuItem = React.forwardRef<HTMLButtonElement, LocalMenuItemProps>(
+  ({ icon, selectedIcon, title, isDisabled = false, isSelected = false, onClick }, ref) => {
+    const iconUI = useMemo(() => {
+      if (isSelected) {
+        return selectedIcon || icon;
+      }
 
-    return icon;
-  }, [icon, isSelected, selectedIcon]);
+      return icon;
+    }, [icon, isSelected, selectedIcon]);
 
-  return (
-    <button
-      className={MENUITEM_CONTAINER_CLASS_NAME({ isSelected })}
-      disabled={isDisabled}
-      onClick={onClick}
-    >
-      {icon && <span className={MENUITEM_ICON_CLASS_NAME({ isSelected })}>{iconUI}</span>}
-      <span className={MENUITEM_TITLE_CLASS_NAME({ isSelected })}>{title}</span>
-    </button>
-  );
-};
+    return (
+      <button
+        className={MENUITEM_CONTAINER_CLASS_NAME({ isSelected })}
+        disabled={isDisabled}
+        onClick={onClick}
+        ref={ref}
+      >
+        {icon && <span className={MENUITEM_ICON_CLASS_NAME({ isSelected })}>{iconUI}</span>}
+        <span className={MENUITEM_TITLE_CLASS_NAME({ isSelected })}>{title}</span>
+      </button>
+    );
+  },
+);
+
+LocalMenuItem.displayName = "LocalMenuItem";
