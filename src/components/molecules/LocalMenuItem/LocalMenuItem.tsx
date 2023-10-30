ee74d00525ce81bc915754652cb8f400ae94831a
Sho Ishikawa <61968429+nuintee@users.noreply.github.com>
2023-10-30 13:30:42 +0900

    LocalMenuItemの追加 (#13)
    * 基本構造作成
    
    * tv化
    
    * preflightの有効化
    
    既存コンポーネントに影響があったため
    
    * selectedIcon対応
    
    * 型修正
    
    * refの受け取り追加
    
    NextLink対応
    
    * 構造修正
    
    * storybook-addon-pseudo-statesの追加
    
    * スタイル・storybook調整
    
    * titleの文字サイズ変更
    
    ---------
    
    Co-authored-by: Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>

---
+++ src/components/molecules/LocalMenuItem/LocalMenuItem.tsx
@@ -0,0 +1,35 @@
+import { useMemo, forwardRef } from "react";
+import {
+  MENUITEM_CONTAINER_CLASS_NAME,
+  MENUITEM_ICON_CLASS_NAME,
+  MENUITEM_TITLE_CLASS_NAME,
+} from "./const";
+import type { LocalMenuItemProps } from "./type";
+
+export const LocalMenuItem = forwardRef<HTMLAnchorElement, LocalMenuItemProps>(
+  ({ id, icon, selectedIcon, title, isDisabled = false, isSelected = false, href }, ref) => {
+    const iconUI = useMemo(() => {
+      if (isSelected) {
+        return selectedIcon || icon;
+      }
+
+      return icon;
+    }, [icon, isSelected, selectedIcon]);
+
+    return (
+      <a
+        className={MENUITEM_CONTAINER_CLASS_NAME({ isSelected, isDisabled })}
+        ref={ref}
+        id={id}
+        {...(!isDisabled && { href })}
+      >
+        {icon && (
+          <span className={MENUITEM_ICON_CLASS_NAME({ isSelected, isDisabled })}>{iconUI}</span>
+        )}
+        <span className={MENUITEM_TITLE_CLASS_NAME({ isSelected, isDisabled })}>{title}</span>
+      </a>
+    );
+  },
+);
+
+LocalMenuItem.displayName = "LocalMenuItem";
