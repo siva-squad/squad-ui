451102633930b7dbf65ea46ad27e5734ab5a1b96
yoheeeei <yohei.contact@gmail.com>
2023-10-31 13:03:54 +0900

    feat(LocalNavigation): :fire: remove state

--- src/components/organisms/LocalNavigation/LocalNavigation.tsx
+++ src/components/organisms/LocalNavigation/LocalNavigation.tsx
@@ -1,12 +1,7 @@
-import { useEffect, useState } from "react";
 import { LocalMenuItem } from "../../molecules/LocalMenuItem";
 import type { LocalNavigationProps } from "./type";
 
 export const LocalNavigation = ({ items, selectedId }: LocalNavigationProps) => {
-  const [selected, setSelected] = useState(selectedId);
-
-  useEffect(() => setSelected(selectedId), [items, selectedId]);
-
   return (
     <nav className="h-full w-fit border-r border-gray-light bg-white px-2 py-4">
       <ul className="flex flex-col gap-2">
@@ -14,7 +9,7 @@ export const LocalNavigation = ({ items, selectedId }: LocalNavigationProps) => 
           <li key={item.id}>
             <LocalMenuItem
               {...item}
-              isSelected={item.id === selected}
+              isSelected={item.id === selectedId}
             />
           </li>
         ))}
