343d5e9c9e5428e60bcca057bc891e8e7a19da6c
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-11-01 17:36:26 +0900

    conditionalPropsの追加

--- src/components/molecules/ListItemDropDown/ListItemDropDown.tsx
+++ src/components/molecules/ListItemDropDown/ListItemDropDown.tsx
@@ -1,23 +1,44 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useMemo, useState } from "react";
 import { ListItem } from "@components/atoms/ListItem";
 import { ListItemProps } from "@components/atoms/ListItem/type";
 import { MergeType } from "@/src/utils/type-utils";
 
 export const ListItemDropDown = ({
   children,
-  href,
-  target,
-  ...itemProps
+  title,
+  id,
+  hasChevron,
+  isDangerText,
+  isDisabled,
+  isSelected,
+  icon,
+  selectedIcon,
+  size,
+  description,
 }: MergeType<ListItemProps, { children?: ReactNode }>) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const conditionalProps = useMemo(() => {
+    if (size === "large") return { size: "large", description } as const;
+
+    return { size };
+  }, [description, size]);
 
   return (
     <div>
       <ListItem
-        {...itemProps}
+        {...{
+          title,
+          id,
+          hasChevron,
+          isDangerText,
+          isDisabled,
+          isSelected,
+          icon,
+          selectedIcon,
+          ...conditionalProps,
+        }}
         onClick={() => setIsOpen((prev) => !prev)}
-        data-ts-avoid-href={href}
-        data-ts-avoid-target={target}
       />
       {isOpen && <>{children}</>}
     </div>
