d34641dbcbdcf66aeeb5d5d2fcd9d832f4f51f90
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-11-06 16:13:56 +0900

    RichMenuListのスクロール対応

--- src/components/organisms/RichMenu/index.tsx
+++ src/components/organisms/RichMenu/index.tsx
@@ -1,11 +1,47 @@
-import { useCallback } from "react";
+import { ReactElement, useCallback } from "react";
 import { Button, ListItem } from "@components/atoms";
+import { ListItemProps } from "@components/atoms/ListItem/type";
 import { ChevronRightIcon, FolderIcon } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/24/solid";
 import { useClientRect } from "@hooks/useClientRect";
 import { useScreenSize } from "@hooks/useScreenSize";
 import { MenuList } from "@molecules/MenuList";
 import { RICH_MENU_CLASS_NAME } from "./const";
 import { RichMenuProps } from "./type";
+
+// TODO: 別ファイル分割
+type RichMenuListProps = {
+  title: string;
+  titleIcon?: ReactElement;
+  items: ListItemProps[];
+};
+const RichMenuList = ({ items, title, titleIcon }: RichMenuListProps) => {
+  return (
+    <article className="w-[240px]">
+      <div className="flex items-center gap-x-1">
+        {titleIcon && <span>{titleIcon}</span>}
+        <span className="text-xs font-medium leading-none">{title}</span>
+      </div>
+      <div className="mt-4 flex h-full flex-col gap-y-2 overflow-auto pb-8">
+        {items.map((item) => (
+          <ListItem
+            {...item}
+            key={item.id}
+          />
+        ))}
+      </div>
+    </article>
+  );
+};
+
+const ITEMS: ListItemProps[] = [...Array(15).keys()].map((_, index) => ({
+  icon: <FolderIcon />,
+  // TODO: ListItemのtruncate対応
+  title: "フォルダ名が入りますフォルダ名が入ります" + index,
+  onClick: () => {},
+  hasChevron: false,
+  size: "medium",
+}));
 
 export const RichMenu = ({
   absolute = false,
@@ -24,66 +60,23 @@ export const RichMenu = ({
       case "list":
         return (
           <div className="flex flex-col gap-y-2 border-l border-gray-extraLight p-4">
-            <div className="flex flex-1 gap-x-4 rounded bg-gray-extraLight p-4">
-              <article>
-                <h3 className="text-xs font-medium leading-none">最近見た</h3>
-                <div className="mt-4 flex flex-col gap-y-2">
-                  <ListItem
-                    key={1}
-                    title="フォルダ名が入りますフォルダ名が入ります"
-                    hasChevron={false}
-                    onClick={() => {}}
-                    icon={<FolderIcon />}
-                    size="medium"
-                  />
-                  <ListItem
-                    key={1}
-                    title="フォルダ名が入りますフォルダ名が入ります"
-                    hasChevron={false}
-                    onClick={() => {}}
-                    icon={<FolderIcon />}
-                    size="medium"
-                  />
-                  <ListItem
-                    key={1}
-                    title="フォルダ名が入りますフォルダ名が入ります"
-                    hasChevron={false}
-                    onClick={() => {}}
-                    icon={<FolderIcon />}
-                    size="medium"
-                  />
-                </div>
-              </article>
+            <div className="flex flex-1 gap-x-4 overflow-hidden rounded bg-gray-extraLight p-4">
+              <RichMenuList
+                items={ITEMS}
+                title="最近見た"
+              />
               <div className="h-full w-px bg-gray-light"></div>
-              <article>
-                <h3 className="text-xs font-medium leading-none">お気に入り</h3>
-                <div className="mt-4 flex flex-col gap-y-2">
-                  <ListItem
-                    key={1}
-                    title="フォルダ名が入りますフォルダ名が入ります"
-                    hasChevron={false}
-                    onClick={() => {}}
-                    icon={<FolderIcon />}
-                    size="medium"
+              <RichMenuList
+                items={ITEMS}
+                title="お気に入り"
+                titleIcon={
+                  <StarIcon
+                    className="text-yellow"
+                    width={12}
+                    height={12}
                   />
-                  <ListItem
-                    key={1}
-                    title="フォルダ名が入りますフォルダ名が入ります"
-                    hasChevron={false}
-                    onClick={() => {}}
-                    icon={<FolderIcon />}
-                    size="medium"
-                  />
-                  <ListItem
-                    key={1}
-                    title="フォルダ名が入りますフォルダ名が入ります"
-                    hasChevron={false}
-                    onClick={() => {}}
-                    icon={<FolderIcon />}
-                    size="medium"
-                  />
-                </div>
-              </article>
+                }
+              />
             </div>
             <div className="w-fit self-center">
               <Button
