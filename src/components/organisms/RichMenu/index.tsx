66a89dc39917712be7a49f39f8ead5569f09ed0d
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-11-06 16:52:48 +0900

    desctiptionの基本レイアウト作成

--- src/components/organisms/RichMenu/index.tsx
+++ src/components/organisms/RichMenu/index.tsx
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 import { Button } from "@components/atoms";
 import { ListItemProps } from "@components/atoms/ListItem/type";
 import { RichMenuList } from "@components/molecules/RichMenulist";
-import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import { ArrowTopRightOnSquareIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import { FolderIcon, StarIcon } from "@heroicons/react/24/solid";
 import { useClientRect } from "@hooks/useClientRect";
 import { useScreenSize } from "@hooks/useScreenSize";
@@ -32,7 +32,36 @@ export const RichMenu = ({
   const RichContentUI = useCallback(() => {
     switch (richMenuType) {
       case "description":
-        return <>desc</>;
+        return (
+          <div className="m-4 flex w-[544px] flex-col px-6 py-4">
+            <div className="flex flex-col items-center gap-y-4">
+              <h2 className="text-lg font-bold leading-tight">
+                一括タグで時間を節約一括タグで時間を節約
+              </h2>
+              <p className="text-xs leading-normal">
+                チーム、グループ、フォルダなどに一括でタグを設置することで簡単に管理ができます！
+                <br />
+                チーム、グループ、フォルダなどに一括でタグを設置することで簡単に管理ができます！タグを設置することで簡単に管理ができます！タグを設置することで簡単に管理ができます！
+              </p>
+            </div>
+            <div className="mt-6 flex-1">
+              <img
+                src="https://placehold.jp/1080x1090.png"
+                className=" object-scale-down"
+              />
+            </div>
+            <div className="mt-4 w-fit self-center">
+              <Button
+                theme="no-background"
+                size="small"
+                iconPosition="right"
+                icon={<ArrowTopRightOnSquareIcon />}
+              >
+                利用方法(FAQ)
+              </Button>
+            </div>
+          </div>
+        );
       case "list":
         return (
           <div className="flex flex-col gap-y-2 border-l border-gray-extraLight p-4">
