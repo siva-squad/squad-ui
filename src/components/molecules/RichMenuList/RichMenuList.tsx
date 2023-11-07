981c4d0b8faa72a9aed9f35112e33b82125e605b
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-11-08 05:27:43 +0900

    RichMenuListの縦線追加

--- src/components/molecules/RichMenuList/RichMenuList.tsx
+++ src/components/molecules/RichMenuList/RichMenuList.tsx
@@ -6,25 +6,28 @@ export const RichMenuList = ({ groups, onClickButton }: RichMenuListProps) => {
   return (
     <div className="my-4 flex max-h-[400px] w-fit flex-col gap-y-2 border-l border-gray-extraLight px-4">
       <div className="flex flex-1 gap-x-4 overflow-hidden rounded bg-gray-extraLight p-4">
-        {groups?.map((group) => (
-          <article
-            className="flex w-[240px] flex-col"
-            key={group.id}
-          >
-            <div className="flex items-center gap-x-1">
-              {group.titleIcon}
-              <span className="text-xs font-medium leading-none">{group.title}</span>
-            </div>
-            <div className="mt-4 flex flex-col gap-y-2 overflow-y-auto overflow-x-hidden pb-4">
-              {group.items.map((item) => (
-                <ListItem
-                  {...item}
-                  key={item.id}
-                />
-              ))}
-            </div>
-            {group.bottomButton && <div className="self-start">{group.bottomButton}</div>}
-          </article>
+        {groups?.map((group, index, passed) => (
+          <>
+            <article
+              className="flex w-[240px] flex-col"
+              key={group.id}
+            >
+              <div className="flex items-center gap-x-1">
+                {group.titleIcon}
+                <span className="text-xs font-medium leading-none">{group.title}</span>
+              </div>
+              <div className="mt-4 flex flex-col gap-y-2 overflow-y-auto overflow-x-hidden pb-4">
+                {group.items.map((item) => (
+                  <ListItem
+                    {...item}
+                    key={item.id}
+                  />
+                ))}
+              </div>
+              {group.bottomButton && <div className="self-start">{group.bottomButton}</div>}
+            </article>
+            {index < passed.length - 1 && <div className="h-full w-px bg-gray-light"></div>}
+          </>
         ))}
       </div>
       <div className="w-fit self-center">
