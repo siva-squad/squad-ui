590ceeb026c58c8b22eba19388929efd8320e006
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-11-08 10:49:08 +0900

    descriptions追加

--- src/components/organisms/RichMenu/dict.ts
+++ src/components/organisms/RichMenu/dict.ts
@@ -4,8 +4,33 @@ import { RichMenuDescriptionProps } from "@components/molecules/RichMenuDescript
 export const DESCRIPTIONS: ({ id: MenuKindKey } & RichMenuDescriptionProps)[] = [
   {
     id: "cvtag",
-    description: "cv",
-    headingText: "cvt",
-    imgSrc: "ea",
+    headingText: "チームやフォルダ、好きな範囲で事前に設置",
+    description:
+      "チーム・フォルダグループ・フォルダ・ページ・お好きな範囲でタグを事前設置。計測タグだけでなく、metaタグの追加も。PR表記や運営者情報など、絶対必要だけど忘れがちな情報を事前に設置して効率化を実現",
+    imgSrc: "https://placehold.jp/1920x1080.png",
+  },
+  {
+    id: "replacement",
+    headingText: "100ページ分のコンテンツも、1分で置換",
+    description: "版権変更や、規約変更時など、過去まで遡ってすべてのページのコンテンツを一括置換",
+    imgSrc: "https://placehold.jp/1920x1080.png",
+  },
+  {
+    id: "media",
+    headingText: "Google広告の必須機能を5分で作成",
+    description: "エンジニアや外注を使わず、すぐに作成と改修が可能に。",
+    imgSrc: "https://placehold.jp/1920x1080.png",
+  },
+  {
+    id: "inspection",
+    headingText: "広告主や代理店のページ表示を完全に管理",
+    description: "審査後のLP変更や、禁止コンテンツが配信されることを完全に防げます",
+    imgSrc: "https://placehold.jp/1920x1080.png",
+  },
+  {
+    id: "form",
+    headingText: "バナーやページごとに見込み客を獲得・管理",
+    description: "",
+    imgSrc: "https://placehold.jp/1920x1080.png",
   },
 ];
