8b7bc73eca53a2407c239b6fa637f17f50f7bc94
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2023-11-09 15:54:10 +0900

    Add more details to tests

--- src/components/atoms/Tooltip/Tooltip.test.tsx
+++ src/components/atoms/Tooltip/Tooltip.test.tsx
@@ -1,6 +1,3 @@
-// One option to work with tests and storybook is to export the stories and use them
-// in order to write tests and avoid duplications.
-// Will run with vitest
 import { composeStories } from "@storybook/react";
 import { render, screen, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
@@ -8,7 +5,7 @@ import { describe, expect, test, afterEach } from "vitest";
 
 import * as TooltipStories from "./Tooltip.stories";
 
-const { DefaultOnHover: Tooltip, FocusOnButton } = composeStories(TooltipStories);
+const { Default: Tooltip, WithButton } = composeStories(TooltipStories);
 
 describe("Tooltip", () => {
   afterEach(() => {
@@ -20,8 +17,11 @@ describe("Tooltip", () => {
     render(<Tooltip />);
 
     const anchor = screen.getByTestId("tooltip-anchor");
+    expect(anchor).toHaveAccessibleName("");
     await user.hover(anchor);
     const tooltip = screen.getByRole("tooltip");
+    expect(anchor).toHaveAccessibleName("メッセージテキスト");
+    expect(tooltip).toHaveAttribute("aria-hidden", "false");
     expect(tooltip).toBeInTheDocument();
     await user.unhover(anchor);
     expect(tooltip).not.toBeInTheDocument();
@@ -31,8 +31,12 @@ describe("Tooltip", () => {
     const user = userEvent.setup();
     render(<Tooltip />);
 
+    const anchor = screen.getByTestId("tooltip-anchor");
+    expect(anchor).toHaveAccessibleName("");
     await user.tab();
     const tooltip = screen.getByRole("tooltip");
+    expect(anchor).toHaveAccessibleName("メッセージテキスト");
+    expect(tooltip).toHaveAttribute("aria-hidden", "false");
     expect(tooltip).toBeInTheDocument();
     await user.tab();
     expect(tooltip).not.toBeInTheDocument();
@@ -40,7 +44,7 @@ describe("Tooltip", () => {
 
   test("native focusable elements should receive focus on tab", async () => {
     const user = userEvent.setup();
-    render(<FocusOnButton />);
+    render(<WithButton />);
 
     const button = screen.getByRole("button");
     const tooltipAnchor = screen.getByTestId("tooltip-anchor");
