497e530c2bb81e1d360695ead27470ba78f7260d
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-11-15 15:51:29 +0900

    useScreenSizeのwindow undefined対策

--- src/hooks/useScreenSize.ts
+++ src/hooks/useScreenSize.ts
@@ -12,11 +12,16 @@ export type ScreenUtilProps = {
 
 export const useScreenSize = (breakpoint = BREAKPOINT): ScreenUtilProps => {
   const [screenSize, setScreenSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 0,
+    height: 0,
   });
 
   useEffect(() => {
+    setScreenSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
     const handleResize = () => {
       setScreenSize({
         width: window.innerWidth,
