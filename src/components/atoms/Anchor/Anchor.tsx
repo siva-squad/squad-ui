1754b72022462ae047ae5b579b2ef99bd6b866db
Lene <51355141+macfrei@users.noreply.github.com>
2023-11-20 17:15:24 +0900

    Task/add forward ref (#33)
    * Add forwardRef and onClick to make Anchor component compatibale with Next Link
    
    * Update package.json

--- src/components/atoms/Anchor/Anchor.tsx
+++ src/components/atoms/Anchor/Anchor.tsx
@@ -1,59 +1,70 @@
+import { forwardRef } from "react";
 import { Spinner } from "../Spinner";
 import { ANCHOR_CHILDREN_CLASS_NAME, ANCHOR_CLASS_NAME, ANCHOR_ICON_CLASS_NAME } from "./const";
 import { AnchorProps } from "./type";
 
-export const Anchor = ({
-  children,
-  icon,
-  size,
-  theme,
-  iconPosition = "left",
-  isDisabled = false,
-  href,
-  isLoading = false,
-}: AnchorProps) => {
-  const showIcon = !isLoading && !!icon;
-  const spinnerProps = {
-    // NOTE: ボタンのサイズが`large`でテキストがない場合はアイコンを大きく表示するため
-    size: size === "large" && children ? "medium" : size,
-    theme: ["red", "primary"].includes(theme) ? "white" : "primary",
-  } as const;
+export const Anchor = forwardRef<HTMLAnchorElement, AnchorProps>(
+  (
+    {
+      children,
+      icon,
+      size,
+      theme,
+      iconPosition = "left",
+      isDisabled = false,
+      href,
+      isLoading = false,
+      onClick,
+    },
+    ref,
+  ) => {
+    const showIcon = !isLoading && !!icon;
+    const spinnerProps = {
+      // NOTE: ボタンのサイズが`large`でテキストがない場合はアイコンを大きく表示するため
+      size: size === "large" && children ? "medium" : size,
+      theme: ["red", "primary"].includes(theme) ? "white" : "primary",
+    } as const;
 
-  return (
-    <a
-      {...(!isDisabled && { href })}
-      className={ANCHOR_CLASS_NAME({
-        theme,
-        isLoading,
-        hasChildren: !!children,
-        size,
-        isDisabled,
-      })}
-    >
-      {showIcon && iconPosition === "left" && (
-        <span className={ANCHOR_ICON_CLASS_NAME({ hasChildren: !!children, size, theme })}>
-          {icon}
-        </span>
-      )}
-      <Spinner
-        loading={isLoading}
-        {...spinnerProps}
-      />
-      {children && (
-        <span
-          className={ANCHOR_CHILDREN_CLASS_NAME({
-            size,
-            theme,
-          })}
-        >
-          {isLoading ? "読み込み中..." : children}
-        </span>
-      )}
-      {showIcon && iconPosition === "right" && (
-        <span className={ANCHOR_ICON_CLASS_NAME({ hasChildren: !!children, size, theme })}>
-          {icon}
-        </span>
-      )}
-    </a>
-  );
-};
+    return (
+      <a
+        {...(!isDisabled && { href })}
+        className={ANCHOR_CLASS_NAME({
+          theme,
+          isLoading,
+          hasChildren: !!children,
+          size,
+          isDisabled,
+        })}
+        ref={ref}
+        onClick={onClick}
+      >
+        {showIcon && iconPosition === "left" && (
+          <span className={ANCHOR_ICON_CLASS_NAME({ hasChildren: !!children, size, theme })}>
+            {icon}
+          </span>
+        )}
+        <Spinner
+          loading={isLoading}
+          {...spinnerProps}
+        />
+        {children && (
+          <span
+            className={ANCHOR_CHILDREN_CLASS_NAME({
+              size,
+              theme,
+            })}
+          >
+            {isLoading ? "読み込み中..." : children}
+          </span>
+        )}
+        {showIcon && iconPosition === "right" && (
+          <span className={ANCHOR_ICON_CLASS_NAME({ hasChildren: !!children, size, theme })}>
+            {icon}
+          </span>
+        )}
+      </a>
+    );
+  },
+);
+
+Anchor.displayName = "Anchor";
