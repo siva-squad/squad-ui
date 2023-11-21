0ade86867d7f899395506a44dfe9c00e9ea4c6c2
Sho Ishikawa <61968429+nuintee@users.noreply.github.com>
2023-11-21 17:53:12 +0900

    input系要素へのref追加 (#34)
    Co-authored-by: Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>

--- src/components/atoms/Checkbox/Checkbox.tsx
+++ src/components/atoms/Checkbox/Checkbox.tsx
@@ -1,24 +1,30 @@
+import { forwardRef } from "react";
 import { CheckIcon } from "@heroicons/react/24/outline";
 import type { CheckboxProps } from "./type";
 
-export const Checkbox = ({ checked, children, value, onChange }: CheckboxProps) => {
-  return (
-    <label className="relative inline-flex cursor-pointer items-center gap-x-2">
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={onChange}
-        className={
-          "peer h-4 w-4 cursor-pointer appearance-none rounded-e rounded-s border border-gray-light checked:border-primary-600 checked:bg-primary-50"
-        }
-        value={value}
-      />
-      <CheckIcon
-        width={12}
-        height={12}
-        className={"absolute left-0.5 hidden text-primary-600 peer-checked:inline-block"}
-      />
-      <span className="font-noto-sans-cjk-jp text-s">{children}</span>
-    </label>
-  );
-};
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ checked, children, value, onChange }, ref) => {
+    return (
+      <label className="relative inline-flex cursor-pointer items-center gap-x-2">
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+          className={
+            "peer h-4 w-4 cursor-pointer appearance-none rounded-e rounded-s border border-gray-light checked:border-primary-600 checked:bg-primary-50"
+          }
+          value={value}
+          ref={ref}
+        />
+        <CheckIcon
+          width={12}
+          height={12}
+          className={"absolute left-0.5 hidden text-primary-600 peer-checked:inline-block"}
+        />
+        <span className="font-noto-sans-cjk-jp text-s">{children}</span>
+      </label>
+    );
+  },
+);
+
+Checkbox.displayName = "Checkbox";
