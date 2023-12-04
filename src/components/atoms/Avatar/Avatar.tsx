3ad66106f3b80a249f7e7d758cc36f00b3fd37d0
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2023-12-04 20:10:07 +0900

    avatarのサイズ調整

--- src/components/atoms/Avatar/Avatar.tsx
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { UserCircleIcon } from "@heroicons/react/24/solid";
+import clsx from "clsx";
 import type { AvatarProps } from "./type";
 
 export const Avatar = ({ src, onClick, size = 32 }: AvatarProps) => {
@@ -13,10 +14,24 @@ export const Avatar = ({ src, onClick, size = 32 }: AvatarProps) => {
         width: size,
       }}
       type="button"
-      className={onClick ? "cursor-pointer" : "cursor-default"}
+      className={clsx(
+        "relative rounded-full bg-gray",
+        onClick ? "cursor-pointer" : "cursor-default",
+      )}
     >
       {!src || isError ? (
-        <UserCircleIcon className="aspect-square rounded-full text-gray ring-4 ring-inset ring-gray" />
+        <>
+          <div
+            className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"
+            style={{ height: size / 1.25, width: size / 1.25 }}
+          ></div>
+          <div
+            className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2"
+            style={{ width: size, height: size }}
+          >
+            <UserCircleIcon className="aspect-square rounded-full text-gray" />
+          </div>
+        </>
       ) : (
         <img
           src={src}
