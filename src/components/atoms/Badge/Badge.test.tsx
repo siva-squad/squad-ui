f13a58c7d24501a9181fe893caa476568430633e
ogaru-sh <ograu.sh@gmail.com>
2023-12-06 10:36:23 +0900

    remove cleanup

--- src/components/atoms/Badge/Badge.test.tsx
+++ src/components/atoms/Badge/Badge.test.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from "@storybook/react";
-import { cleanup, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, expect } from "vitest";
 
 import * as BadgeStories from "./Badge.stories";
@@ -7,9 +7,6 @@ import * as BadgeStories from "./Badge.stories";
 const { Default: Badge } = composeStories(BadgeStories);
 
 describe("Badge", () => {
-  afterEach(() => {
-    cleanup();
-  });
   test("renders with children", () => {
     render(<Badge>Test</Badge>);
     const badge = screen.getByText("Test");
