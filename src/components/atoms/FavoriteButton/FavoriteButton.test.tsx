5af5d4a8ef394dc7559c15bac71c3e6f4985c160
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2023-12-14 14:17:43 +0100

    Add tests

--- src/components/atoms/FavoriteButton/FavoriteButton.test.tsx
+++ src/components/atoms/FavoriteButton/FavoriteButton.test.tsx
@@ -1,5 +1,6 @@
 import { composeStories } from "@storybook/react";
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { describe, expect, test } from "vitest";
 
 import * as FavoriteButtonStories from "./FavoriteButton.stories";
@@ -7,9 +8,38 @@ import * as FavoriteButtonStories from "./FavoriteButton.stories";
 const { Default: FavoriteButton } = composeStories(FavoriteButtonStories);
 
 describe("FavoriteButton", () => {
-
   test("renders", async () => {
     render(<FavoriteButton />);
+
+    const button = screen.getByRole("button", { name: "フォルダ１をお気に入りに追加" });
+
+    expect(button).toBeInTheDocument();
   });
 
+  test("has different label when isFavourite is true", async () => {
+    render(<FavoriteButton isFavorite={true} />);
+
+    const button = screen.getByRole("button", { name: "フォルダ１をお気に入りから削除" });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test("calls callback function on button click", async () => {
+    const mock = vi.fn();
+    render(<FavoriteButton onClick={mock} />);
+
+    const button = screen.getByRole("button", { name: "フォルダ１をお気に入りに追加" });
+
+    await userEvent.click(button);
+
+    expect(mock).toHaveBeenCalled();
+  });
+
+  test("has disabled as an attribute when passed as props", () => {
+    render(<FavoriteButton disabled={true} />);
+
+    const button = screen.getByRole("button", { name: "フォルダ１をお気に入りに追加" });
+
+    expect(button).toHaveAttribute("disabled");
+  });
 });
