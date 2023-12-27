7f8de62387c26461928debb597900ac4c82af0f3
yoheeeei <yohei.contact@gmail.com>
2023-12-27 12:04:23 +0900

    feat(BreadcrumbItem):✨Update styles

--- src/components/atoms/BreadcrumbItem/BreadcrumbItem.tsx
+++ src/components/atoms/BreadcrumbItem/BreadcrumbItem.tsx
@@ -4,15 +4,16 @@ import type { BreadcrumbItemProps } from "./type";
 
 export const BreadcrumbItem = ({ children }: BreadcrumbItemProps) => {
   return (
-    <div
+    <li
       className={clsx(
-        "text-xs font-regular leading-none text-gray-dark",
+        "cursor-pointer list-none text-xs font-regular leading-none text-gray-dark",
         "hover:underline",
         "focus:underline",
         "active:text-gray-extraDark active:underline",
+        "last:pointer-events-none last:cursor-default last:text-gray-extraDark last:no-underline",
       )}
     >
       {children}
-    </div>
+    </li>
   );
 };
