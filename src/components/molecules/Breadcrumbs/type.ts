b291a4e7071dcbf47b4d87a8434f0b20d7be4624
yoheeeei <yohei.contact@gmail.com>
2024-01-04 15:43:56 +0900

    feat(Breadcrumbs): :sparkles: Add overflow

--- src/components/molecules/Breadcrumbs/type.ts
+++ src/components/molecules/Breadcrumbs/type.ts
@@ -1,2 +1,7 @@
+import { ReactElement, ReactNode } from "react";
+import { BreadcrumbItemProps } from "@components/atoms/BreadcrumbItem/type";
+
 export type BreadcrumbsProps = {
-}
+  children: ReactElement<BreadcrumbItemProps>[];
+  separator?: ReactNode;
+};
