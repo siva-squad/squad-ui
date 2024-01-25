a2a7120e858f1e46c974918ac83e1986c16b54f5
Tomoki Shimizu <101984097+marromugi@users.noreply.github.com>
2024-01-25 12:14:47 +0900

    fix: :lipstick: SearchInput: スタイル修正 (#71)
    * fix: :lipstick: SearchInput: fix some styles
    
    * package.json: update version

--- src/components/atoms/SearchInput/const.ts
+++ src/components/atoms/SearchInput/const.ts
@@ -3,8 +3,9 @@ import { tv } from "tailwind-variants";
 
 export const SEARCH_INPUT_CLASS_NAME = tv({
   base: clsx(
-    "block w-full rounded-3xl border border-gray-extraLight py-4 pl-9 pr-4 text-s text-black",
-    "focus:border-gray focus:outline-none",
+    "block w-full rounded-lg border border-gray-extraLight py-4 pl-9 pr-4 text-s text-black",
+    "focus:border-primary-600 focus:outline-none",
+    'placeholder:text-gray-dark'
   ),
   variants: {
     inputSize: {
