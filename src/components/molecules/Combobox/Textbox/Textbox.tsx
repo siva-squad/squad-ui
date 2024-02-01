07a018cdd2fa7804616875da398b6e290967c6c4
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2024-02-01 13:29:10 +0900

    First input version

--- src/components/molecules/Combobox/Textbox/Textbox.tsx
+++ src/components/molecules/Combobox/Textbox/Textbox.tsx
@@ -1,5 +1,16 @@
+import clsx from "clsx";
 import type { TextboxProps } from "./type";
 
 export const Textbox = ({}: TextboxProps) => {
-  return <></>;
+  return (
+    <input
+      type="text"
+      className={clsx(
+        "rounded-lg border border-gray-light bg-white p-4 text-sm leading-none outline-none w-full",
+        "ring-offset-2 focus:text-black focus:ring-1 focus:ring-primary-600",
+        "disabled:cursor-not-allowed disabled:text-gray",
+      )}
+      placeholder="選択してください"
+    />
+  );
 };
