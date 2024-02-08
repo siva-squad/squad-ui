7737069521b3ab0cb5a31abb1712e28e9ada28a7
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2024-02-08 10:55:50 +0900

    Add longer story

--- src/components/molecules/Combobox/Combobox.stories.tsx
+++ src/components/molecules/Combobox/Combobox.stories.tsx
@@ -14,30 +14,68 @@ export const Default: StoryObj<typeof Combobox> = {
   args: {
     listName: "Test list",
     disabled: false,
-    inputLabelId: "123",
     placeholder: "Option 456",
     size: "normal",
     isRequired: true,
     options: [
       {
         id: "1",
-        isSelected: false,
         name: "Option 1",
       },
       {
         id: "2",
-        isSelected: false,
         name: "Option 2",
       },
       {
         id: "3",
-        isSelected: false,
         name: "Option 3",
       },
       {
         id: "4",
-        isSelected: false,
         name: "Option 4",
+      },
+    ],
+  },
+};
+
+export const LongList: StoryObj<typeof Combobox> = {
+  args: {
+    listName: "Test list 2",
+    disabled: false,
+    size: "normal",
+    isRequired: true,
+    options: [
+      {
+        id: "1",
+        name: "Option 1",
+      },
+      {
+        id: "2",
+        name: "Option 2",
+      },
+      {
+        id: "3",
+        name: "Option 3",
+      },
+      {
+        id: "4",
+        name: "Option 4",
+      },
+      {
+        id: "12",
+        name: "Option 12",
+      },
+      {
+        id: "22",
+        name: "Option 22",
+      },
+      {
+        id: "32",
+        name: "Option 32",
+      },
+      {
+        id: "42",
+        name: "Option 42",
       },
     ],
   },
