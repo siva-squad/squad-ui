3ac67dbaf501178a16c4bc4a6ca2c735c498036b
Sho Ishikawa <sho-ft@ShonoMacBook-Pro.local>
2024-02-09 14:35:48 +0900

    maxCountのデフォルトを4に変更

--- src/components/molecules/AvatarMultiple/AvatarMultiple.tsx
+++ src/components/molecules/AvatarMultiple/AvatarMultiple.tsx
@@ -4,7 +4,7 @@ import type { AvatarMultipleProps } from "./type";
 
 export const AvatarMultiple = ({
   avatars,
-  maxDisplayCount = 3,
+  maxDisplayCount = 4,
   onClick,
   showIndicator = true,
 }: AvatarMultipleProps) => {
