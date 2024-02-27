07e7dfd4b8574c64d6c7e92ddb445b4c10158077
Marie-Carleen Koizumi <mariecarleen.koizumi@gmail.com>
2024-02-27 16:39:40 +0900

    Update button types

--- src/components/atoms/Anchor/type.ts
+++ src/components/atoms/Anchor/type.ts
@@ -1,12 +1,12 @@
 import { ComponentProps, ReactNode } from "react";
 
-type Theme = "white" | "primary" | "red" | "gray" | "no-background" | "underline";
+type Theme = "primary" | "red" | "gray";
 type Size = "small" | "medium" | "large";
-type Background = "filled" | "white";
+type Variant = "fill" | "outline" | "text" | "underline";
 
 export type AnchorProps = Pick<ComponentProps<"a">, "onClick" | "children"> & {
   icon?: ReactNode;
-  background?: Background;
+  variant: Variant;
   theme: Theme;
   size: Size;
   loading?: boolean;
