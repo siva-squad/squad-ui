f931fb760b203a2484ac01404ac27275f7d1a5a8
yoheeeei <yohei.contact@gmail.com>
2024-03-14 15:37:51 +0900

    feat(Selector): :lipstick: Add scroll to list

--- src/components/atoms/Selector/SelectorList/SelectorList.tsx
+++ src/components/atoms/Selector/SelectorList/SelectorList.tsx
@@ -8,19 +8,21 @@ export const SelectorList = <OptionValue extends BaseOptionValue>({
   onClick,
 }: SelectorListProps<OptionValue>) => {
   return (
-    <ul
-      role="listbox"
-      className="z-[1] flex w-full flex-col items-start justify-start rounded bg-white py-1 shadow-03"
-    >
-      {options.map((option) => (
-        <SelectorListItem
-          key={String(option.value)}
-          option={option}
-          isActive={option.value === value}
-          disabled={option.disabled}
-          onClick={onClick}
-        />
-      ))}
-    </ul>
+    <div className="z-[1] flex h-52 w-full flex-col items-start justify-start rounded bg-white py-1 shadow-03">
+      <ul
+        role="listbox"
+        className="hidden-scrollbar w-full overflow-y-scroll"
+      >
+        {options.map((option) => (
+          <SelectorListItem
+            key={String(option.value)}
+            option={option}
+            isActive={option.value === value}
+            disabled={option.disabled}
+            onClick={onClick}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
